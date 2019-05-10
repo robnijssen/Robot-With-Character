2f0bcecdf7dfcb661f364589ef7737c47a11a9ac
robnijssen <49899007+robnijssen@users.noreply.github.com>
2019-05-10 11:27:28 +0200

    Update statemachine_play.py

--- close_encounters_ur5/scripts/statemachine_play.py
+++ close_encounters_ur5/scripts/statemachine_play.py
@@ -30,7 +30,7 @@ class Constants:
 
 class Variables:
     # a variable to keep track of what state the control is in
-    cmd_state = 1
+    playCmd_state = 1
     # a variable to keep track if the board is set up or not
     board_set_up = False
     # a variable to keep track if a person is detected
@@ -38,7 +38,7 @@ class Variables:
     # a variable to keep track of the turns
     turn_number = 0
     # a variable to keep track of how far away the face is
-    distance_to_face = 0
+    playDistance_to_face = 0
     # score variables
     fb_player_score = 0
     fb_bot_score = 0
@@ -49,14 +49,14 @@ class Variables:
 
 class Callbacks:
     def state(self, state):
-        variables.cmd_state = state.data
-    def distance_to_face(self, distance):
-        variables.distance_to_face = distance.data
-    def vision_score(self, score):
-        if variables.turn_number % 2 != 0:
-            variables.fb_player_score = score.data
+        playVariables.playCmd_state = state.data
+    def playDistance_to_face(self, distance):
+        playVariables.playDistance_to_face = distance.data
+    def playVision_score(self, score):
+        if playVariables.turn_number % 2 != 0:
+            playVariables.fb_player_score = score.data
         else:
-            variables.fb_bot_score = score.data
+            playVariables.fb_bot_score = score.data
 
 # state machine
 
@@ -71,17 +71,17 @@ class Idle(State):
     def transitionRun(self):
         rospy.loginfo("Play: Not active.")
         # reset values
-        variables.turn_number = 1
-        variables.player_total_score = 0
-        variables.bot_total_score = 0
+        playVariables.turn_number = 1
+        playVariables.player_total_score = 0
+        playVariables.bot_total_score = 0
     def mainRun(self):
         # publish state 0
         cmd_play_publisher.publish(0)
         # publish feedback 0
         fb_play_publisher.publish(0)
-        rospy.sleep(constants.sleeptime)
+        rospy.sleep(playConstants.sleeptime)
     def next(self):
-        if(variables.cmd_state == 2):
+        if(playVariables.playCmd_state == 2):
             return PlayMachine.setUpBoard
         else:
             return PlayMachine.idle
@@ -92,67 +92,67 @@ class SetUpBoard(State):
     def mainRun(self):
         # publish command 1
         cmd_play_publisher.publish(1)
-        variables.board_set_up = True
+        playVariables.board_set_up = True
     def next(self):
         return PlayMachine.check
 
 class Check(State):
     def transitionRun(self):
         rospy.loginfo("Play: Checking for people and/or score")
-        if variables.turn_number % 2 == 0:
-            if variables.fb_player_score > variables.fb_bot_score:
+        if playVariables.turn_number % 2 == 0:
+            if playVariables.fb_player_score > playVariables.fb_bot_score:
                 # player won this round
-                variables.player_total_score += 1
-            elif variables.fb_player_score < variables.fb_bot_score:
+                playVariables.player_total_score += 1
+            elif playVariables.fb_player_score < playVariables.fb_bot_score:
                 # bot won this round
-                variables.bot_total_score += 1
+                playVariables.bot_total_score += 1
             else:
                 # tie
                 # to do: implement tie and replaying the round
                 # for now, this is counted as a bot win
-                variables.bot_total_score += 1
+                playVariables.bot_total_score += 1
     def mainRun(self):
         # do checks
-        variables.person_detected = False
+        playVariables.person_detected = False
         '''
         while not variables.fb_check_for_people_and_score_done == 1:
-            if variables.distance_to_face > 0:
+            if variables.playDistance_to_face > 0:
                 variables.person_detected = True
         '''
-        if variables.distance_to_face > 0:
-            variables.person_detected = True
+        if playVariables.playDistance_to_face > 0:
+            playVariables.person_detected = True
     def next(self):
-        if variables.person_detected == False:
-            if variables.turn_number <= constants.max_turns:
+        if playVariables.person_detected == False:
+            if playVariables.turn_number <= playConstants.max_turns:
                 # person left during the game
                 fb_play_publisher.publish(1)
-                rospy.sleep(constants.sleeptime)
+                rospy.sleep(playConstants.sleeptime)
                 return PlayMachine.idle
-            elif variables.turn_number == constants.max_turns + 1:
+            elif playVariables.turn_number == playConstants.max_turns + 1:
                 # person left after the game
                 fb_play_publisher.publish(3)
-                rospy.sleep(constants.sleeptime)
+                rospy.sleep(playConstants.sleeptime)
                 return PlayMachine.idle
             else:
-                fatal = "Play: Turn number can't be this high" + str(variables.turn_number)
+                fatal = "Play: Turn number can't be this high" + str(playVariables.turn_number)
                 rospy.logfatal(fatal)
                 return PlayMachine.idle
         else:
-            if variables.player_total_score == 2:
+            if playVariables.player_total_score == 2:
                 # player won
                 fb_play_publisher.publish(2)
                 final_score.publish(0)
-                rospy.sleep(constants.sleeptime)
+                rospy.sleep(playConstants.sleeptime)
                 return PlayMachine.idle
-            elif variables.bot_total_score == 2:
+            elif playVariables.bot_total_score == 2:
                 # bot won
                 fb_play_publisher.publish(2)
                 final_score.publish(1)
-                rospy.sleep(constants.sleeptime)
+                rospy.sleep(playConstants.sleeptime)
                 return PlayMachine.idle
             else:
-                if variables.board_set_up == True:
-                    if variables.turn_number % 2 != 0:
+                if playVariables.board_set_up == True:
+                    if playVariables.turn_number % 2 != 0:
                         # uneven turn_number --> player's turn
                         return PlayMachine.waitForPlayersMove
                     else:
@@ -167,13 +167,13 @@ class WaitForPlayersMove(State):
     def mainRun(self):
         # publish command 2
         cmd_play_publisher.publish(2)
-        rospy.sleep(constants.sleeptime)
-        rospy.sleep(constants.debugtime)
+        rospy.sleep(playConstants.sleeptime)
+        rospy.sleep(playConstants.debugtime)
     def next(self):
-        if variables.fb_player_score != 0:
+        if playVariables.fb_player_score != 0:
             return PlayMachine.waitForPlayersMove
         else:
-            variables.turn_number += 1
+            playVariables.turn_number += 1
             return PlayMachine.check
 
 class TakeTurn(State):
@@ -182,13 +182,13 @@ class TakeTurn(State):
     def mainRun(self):
         # publish command 3
         cmd_play_publisher.publish(3)
-        rospy.sleep(constants.sleeptime)
-        rospy.sleep(constants.debugtime)
+        rospy.sleep(playConstants.sleeptime)
+        rospy.sleep(playConstants.debugtime)
     def next(self):
-        if variables.fb_bot_score != 0:
+        if playVariables.fb_bot_score != 0:
             return PlayMachine.takeTurn
         else:
-            variables.turn_number += 1
+            playVariables.turn_number += 1
             return PlayMachine.check
             
 if __name__ == '__main__':
@@ -214,12 +214,12 @@ if __name__ == '__main__':
         final_score = rospy.Publisher('/final_score', Int8, queue_size=1)
 
         # init subscriber
-        variables = Variables()
-        callbacks = Callbacks()
-        constants = Constants()
-        cmd_state = rospy.Subscriber("/cmd_state", Int8, callbacks.state)
-        distance_to_face = rospy.Subscriber("/vision_face_d", Int8, callbacks.distance_to_face)
-        vision_score = rospy.Subscriber("/vision_score", Int8, callbacks.vision_score)
+        playVariables = Variables()
+        playCallbacks = Callbacks()
+        playConstants = Constants()
+        playCmd_state = rospy.Subscriber("/cmd_state", Int8, playCallbacks.state)
+        playDistance_to_face = rospy.Subscriber("/vision_face_d", Int8, playCallbacks.distance_to_face)
+        playVision_score = rospy.Subscriber("/vision_score", Int8, playCallbacks.vision_score)
 
         # instantiate state machine
         #<statemachine_name>.<state_without_capital_letter> = <state_class_name>()
